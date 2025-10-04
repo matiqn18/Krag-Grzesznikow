d47611233f8475844d966e5d19f2e79c9e60acd3
Skeletonek <l.plich@skeletonek.com>
2025-10-04 15:32:32 +0200

    Base main menu

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
-func _ready() -> void:
-    pass
+const GAME_SCENE_PATH := "res://scenes/game_scene.tscn"
 
-func _update() -> void:
-    pass
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file(GAME_SCENE_PATH)
 
-func _play_button_press() -> void:
-    get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_master_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(0, value)
