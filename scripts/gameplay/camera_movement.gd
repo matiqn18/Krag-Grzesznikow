e564363b2089f61ad425074a860500b12d4c4f66
Skeletonek <l.plich@skeletonek.com>
2025-10-04 16:27:06 +0200

    Move mouse_sensitivity to exportable var

--- scripts/gameplay/camera_movement.gd
+++ scripts/gameplay/camera_movement.gd
@@ -1,13 +1,15 @@
 extends Camera3D
 
+@export var mouse_sensitivity := 0.01
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			rotation.y -= event.relative.x * 0.01
-			rotation.x -= event.relative.y * 0.01
-			rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
+    if event is InputEventMouseButton:
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    elif event.is_action_pressed("ui_cancel"):
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+    if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+        if event is InputEventMouseMotion:
+            rotation.y -= event.relative.x * mouse_sensitivity
+            rotation.x -= event.relative.y * mouse_sensitivity
+            rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
