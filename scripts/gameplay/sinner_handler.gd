861bcbaea967fd9ae7159ca19106ab20dde90f2f
Skeletonek <l.plich@skeletonek.com>
2025-10-05 09:15:11 +0200

    Add dropping the sinner

--- scripts/gameplay/sinner_handler.gd
+++ scripts/gameplay/sinner_handler.gd
@@ -5,16 +5,25 @@ extends Node3D
 var active_sinner: SinnerNode
 var available_sinners: Array[Node]
 
+var sinners_left := 4
 
 func _ready() -> void:
+    GameManager.drop_the_sinner.connect(drop_the_sinner)
     available_sinners = get_node("AvailableSinners").get_children()
     enter_sinner()
 
 func enter_sinner() -> void:
-    active_sinner = choose_a_sinner()
-    active_sinner.reparent(get_node("CurrentSinner"))
-    animation_player.play("sinner_enter")
-    active_sinner.animation_player.play("Walk")
+    if sinners_left > 0:
+        sinners_left -= 1
+        active_sinner = choose_a_sinner()
+        active_sinner.reparent(get_node("CurrentSinner"))
+        active_sinner.position = Vector3(0,0,0)
+        active_sinner.rotation = Vector3(0,0,0)
+        animation_player.play("sinner_enter")
+        active_sinner.animation_player.play("Walk")
+    else:
+        #Game over
+        pass
 
 func stop_walking() -> void:
     active_sinner.animation_player.stop(true)
@@ -24,3 +33,16 @@ func start_dialogic(timeline: String) -> void:
 
 func choose_a_sinner() -> Node3D:
     return available_sinners.pick_random()
+
+func drop_the_sinner() -> void:
+    animation_player.play("sinner_exit")
+    animation_player.animation_finished.connect(next_sinner)
+
+#HACK: Unused arg is a hack for the AnimationPlayer.animation_finished calling signals with the argument
+func next_sinner(_unused_arg = "") -> void:
+    animation_player.animation_finished.disconnect(next_sinner)
+    active_sinner.queue_free()
+    # Update sinner list
+    available_sinners = get_node("AvailableSinners").get_children()
+    enter_sinner()
+
